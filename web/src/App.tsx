3c4906e58625a2d0becdd2f739e60b3e34c735df
Daniel Hallgren <danielhallgren12@gmail.com>
2026-03-06 10:08:55 +0100

    Add dark map tiles (CartoDB Dark Matter) with subtle neon glow effect

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -253,8 +253,8 @@ function App() {
               >
                 <MapEvents onZoomChange={setZoomLevel} />
                 <TileLayer
-                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+                  url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png"
                 />
                 
                 {(() => {
